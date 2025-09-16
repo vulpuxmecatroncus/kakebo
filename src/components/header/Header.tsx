1ce28f5c9462c40477c4c521390df795b3081066
yasuomaidana <yasuomaidana@gmail.com>
2025-09-15 20:12:10 -0600

    Refactor Header component to remove SpeedDial and integrate UserConfigs for user actions

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,26 +1,16 @@
 import React, {Dispatch, SetStateAction} from 'react';
 
-import {AccountCircle, Home} from "@mui/icons-material";
-import {Box, IconButton, SpeedDial, SpeedDialAction, Typography} from "@mui/material";
+import Home from "@mui/icons-material/Home";
+import {Box, IconButton, Typography} from "@mui/material";
 //SpeedDialIcon,
 import MenuIcon from "@mui/icons-material/Menu";
 import {useTranslation} from 'react-i18next';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-//import ShareIcon from '@mui/icons-material/Share';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LanguageIcon from '@mui/icons-material/Language';
-import {ChangeLanguage} from "./changeLanguage/ChangeLanguage.tsx";
+import {ChangeLanguage} from "./ChangeLanguage.tsx";
+import UserConfigs from "./UserConfigs.tsx";
 
 const Header = ({setDrawerOpen}: { setDrawerOpen: Dispatch<SetStateAction<boolean>> }) => {
 
     const [open, setOpen] = React.useState(false);
-    const actions = [
-        {icon: <AccountCircle/>, name: 'Copy'},
-        {icon: <SaveIcon/>, name: 'Save'},
-        {icon: <PrintIcon/>, name: 'Print'},
-        {icon: <LanguageIcon/>, name: 'Language'},
-    ];
 
     return (
         <Box component="header" boxShadow={1}>
@@ -34,28 +24,7 @@ const Header = ({setDrawerOpen}: { setDrawerOpen: Dispatch<SetStateAction<boolea
                 <Typography variant="h5" sx={{flexGrow: 1, textAlign: "center"}}>
                     {useTranslation().t('title')}
                 </Typography>
-                <Box display="flex" alignItems="center" position="relative" height={56}>
-                    <Box position="absolute" right={0} top={0} zIndex={2}>
-                        <SpeedDial
-                            ariaLabel="SpeedDial basic example"
-                            icon={<SettingsIcon/>}
-                            direction="down"
-                        >
-                            {actions.map((action) => (
-                                <SpeedDialAction
-                                    key={action.name}
-                                    icon={action.icon}
-                                    slotProps={{
-                                        tooltip: {
-                                            title: action.name,
-                                        },
-                                    }}
-                                    onClick={action.name === 'Language' ? () => setOpen(true) : undefined}
-                                />
-                            ))}
-                        </SpeedDial>
-                    </Box>
-                </Box>
+                <UserConfigs setOpen={setOpen}/>
             </Box>
             <ChangeLanguage open={open} setOpen={setOpen}/>
         </Box>
