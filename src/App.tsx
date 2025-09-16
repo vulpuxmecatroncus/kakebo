d4376319a4e1f77188b0cb78d3ed9ba7a7870c47
yasuomaidana <yasuomaidana@gmail.com>
2025-09-16 16:51:06 -0600

    Replace Drawer component with HamburgerMenu for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import {useState} from "react";
 //import {invoke} from "@tauri-apps/api/core";
 import {ThemeProvider} from '@mui/material/styles';
-import {
-    Box,
-    Button, Divider,
-    Drawer,
-    Grid,
-    List, ListItem,
-    ListItemButton, ListItemIcon, ListItemText,
-    Typography
-} from "@mui/material";
-
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import {Box, Button, Grid, Typography} from "@mui/material";
 
 
 import "./App.scss";
 import {theme} from "./Theme.ts";
 import Header from "./components/header/Header.tsx";
 import MonthSelector from "./components/MonthSelector.tsx";
+import HamburgerMenu from "./components/HamburgerMenu.tsx";
 
 function App() {
     // const [greetMsg, setGreetMsg] = useState("");
@@ -35,33 +25,7 @@ function App() {
                 <Header setDrawerOpen={setDrawerOpen}/>
                 <Box display="flex" height="90vh">
                     {/* Hamburger Drawer */}
-                    <Drawer anchor="left" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
-                        <List>
-                            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                                <ListItem key={text} disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
-                                        </ListItemIcon>
-                                        <ListItemText primary={text}/>
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                        <Divider/>
-                        <List>
-                            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                                <ListItem key={text} disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
-                                        </ListItemIcon>
-                                        <ListItemText primary={text}/>
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Drawer>
+                    <HamburgerMenu drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen}/>
                     {/* Main Content */}
                     <Box flex={1} display="flex" flexDirection="column" justifyContent="space-between" p={2}>
                         <Box flex={1}>
