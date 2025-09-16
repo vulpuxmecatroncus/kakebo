530928fe04c75a85391543618fe234c61a4c0c71
yasuomaidana <yasuomaidana@gmail.com>
2025-09-16 12:25:07 -0600

    Refactor UserConfigs component to manage SpeedDial state and update language selection handling

--- src/components/header/UserConfigs.tsx
+++ src/components/header/UserConfigs.tsx
@@ -5,9 +5,10 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import PrintIcon from "@mui/icons-material/Print";
 import LanguageIcon from "@mui/icons-material/Language";
 import SaveIcon from "@mui/icons-material/Save";
-import {Dispatch, SetStateAction} from "react";
+import {Dispatch, SetStateAction, useState} from "react";
 
-const UserConfigs = ({setOpen}: { setOpen: Dispatch<SetStateAction<boolean>> }) => {
+const UserConfigs = ({openChangeLanguage}: { openChangeLanguage: Dispatch<SetStateAction<boolean>> }) => {
+    const [openSpeed, setOpenSpeed] = useState<boolean>(false);
     const actions = [
         {icon: <AccountCircle/>, name: 'Copy'},
         {icon: <SaveIcon/>, name: 'Save'},
@@ -16,11 +17,19 @@ const UserConfigs = ({setOpen}: { setOpen: Dispatch<SetStateAction<boolean>> }) 
     ];
     return (
         <Box display="flex" alignItems="center" position="relative" height={56}>
-            <Box position="absolute" right={0} top={0} zIndex={2}>
+            <Box position="absolute" right={0} top={0}>
                 <SpeedDial
                     ariaLabel="SpeedDial basic example"
                     icon={<SettingsIcon/>}
                     direction="down"
+                    open={openSpeed}
+                    onOpen={() => setOpenSpeed(true)}
+                    onClose={(_event, reason) => {
+                        if (reason !== "mouseLeave") {
+                            setOpenSpeed(false);
+                        }
+                    }
+                    }
                 >
                     {actions.map((action) => (
                         <SpeedDialAction
@@ -31,7 +40,11 @@ const UserConfigs = ({setOpen}: { setOpen: Dispatch<SetStateAction<boolean>> }) 
                                     title: action.name,
                                 },
                             }}
-                            onClick={() => action.name === 'Language' && setOpen(true)}
+                            onClick={() => {
+                                setOpenSpeed(false);
+                                action.name === 'Language' && openChangeLanguage(true);
+                            }
+                            }
                         />
                     ))}
                 </SpeedDial>
