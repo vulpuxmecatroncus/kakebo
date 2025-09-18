1f38fc63c0fba42f27bd601936d1a756b66642f8
yasuomaidana <yasuomaidana@gmail.com>
2025-09-17 21:21:30 -0600

    Add Footer component for consistent page structure

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+//import React from 'react';
+
+import {Container} from "@mui/material";
+
+const Footer = () => {
+    return (
+        <Container component="footer" sx={{textAlign: 'center'}}>
+            Footer
+        </Container>
+    );
+};
+
+export default Footer;
