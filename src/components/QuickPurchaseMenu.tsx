17e73ee15154b59df7d4f1bdce968acda02bd3ee
yasuomaidana <yasuomaidana@gmail.com>
2025-09-18 18:55:57 -0600

    Add QuickPurchaseMenu component with Stack layout for organized display

---
+++ src/components/QuickPurchaseMenu.tsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+import {Box, Stack} from "@mui/material";
+
+const QuickPurchaseMenu = () => {
+    return (
+        <Stack>
+            <Box>Row 2</Box>
+            <Box>Row 1</Box>
+        </Stack>
+    );
+};
+
+export default QuickPurchaseMenu;
