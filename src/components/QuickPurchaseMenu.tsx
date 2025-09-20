7f441d7bc5c58736da8813717e39fe3764e10f34
yasuomaidana <yasuomaidana@gmail.com>
2025-09-20 14:54:05 -0600

    Enhance QuickPurchaseMenu with long press functionality and new icons

--- src/components/QuickPurchaseMenu.tsx
+++ src/components/QuickPurchaseMenu.tsx
@@ -1,12 +1,40 @@
-// import React from 'react';
+import {useState} from "react";
 
-import {Box, Stack} from "@mui/material";
+import {Box, Fab, Stack} from "@mui/material";
+
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import StoreIcon from '@mui/icons-material/Store';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import AddIcon from '@mui/icons-material/Add';
 
 const QuickPurchaseMenu = () => {
+    const [longPressed, setLongPressed] = useState(false);
     return (
         <Stack>
-            <Box>Row 2</Box>
-            <Box>Row 1</Box>
+            <Box sx={{'& > :not(style)': {m: 2}}}>
+                <Fab color="primary" aria-label="add" size="medium">
+                    <RestaurantIcon/>
+                </Fab>
+                <Fab
+                    color="primary"
+                    aria-label="edit"
+                    size="large"
+                    onPointerDown={e => {
+                        if (e.pointerType === 'touch' || e.pointerType === 'mouse') {
+                            const timer = setTimeout(() => setLongPressed(true), 500);
+                            e.target.addEventListener('pointerup', () => {
+                                clearTimeout(timer);
+                                setLongPressed(false);
+                            }, {once: true});
+                        }
+                    }}
+                >
+                    {longPressed ? <StoreIcon/> : <AddIcon/>}
+                </Fab>
+                <Fab color="primary" aria-label="edit" size="medium">
+                    <LocalGroceryStoreIcon/>
+                </Fab>
+            </Box>
         </Stack>
     );
 };
