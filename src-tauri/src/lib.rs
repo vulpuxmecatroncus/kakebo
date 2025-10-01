d0df256dedcbfd011bd0e566ed1354c5d8b56b35
yasuomaidana <yasuomaidana@gmail.com>
2025-09-30 21:09:46 -0600

    Establish database connection during application setup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,20 @@
+use kakebo_data_model::db;
+use kakebo_data_model::db::establish_db_connection;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
+    establish_db_connection();
     format!("Hello, {}! You've been greeted from Rust! ATTE: YM2", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|_app| {
+            db::init();
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
